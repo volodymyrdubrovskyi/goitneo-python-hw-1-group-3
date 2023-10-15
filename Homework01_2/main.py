71839ed9f4ecd15b3be6685cbf66cd44c75b9254
volodymyrdubrovskyi <vladimir.dubrovskiy@gmail.com>
2023-10-15 22:57:13 +0300

    homework 01 v15.10.2023

--- Homework01_2/main.py
+++ Homework01_2/main.py
@@ -40,21 +40,24 @@ def main():
     print("Welcome to the assistant bot!")
     while True:
         user_input = input("Enter a command: ")
-        command, *args = parse_input(user_input)
+        if user_input:
+            command, *args = parse_input(user_input)
 
-        if command in ["close", "exit"]:
-            print("Good bye!")
-            break
-        elif command == "hello":
-            print("How can I help you?")
-        elif command == "add":
-            print(add_contact(args, contacts))
-        elif command == "change":
-            print(change_contact(args, contacts))
-        elif command == "all":
-            print(show_all(args, contacts))
-        elif command == "phone":
-            print(show_phone(args, contacts))
+            if command in ["close", "exit"]:
+                print("Good bye!")
+                break
+            elif command == "hello":
+                print("How can I help you?")
+            elif command == "add":
+                print(add_contact(args, contacts))
+            elif command == "change":
+                print(change_contact(args, contacts))
+            elif command == "all":
+                print(show_all(args, contacts))
+            elif command == "phone":
+                print(show_phone(args, contacts))
+            else:
+                print("Invalid command.")
         else:
             print("Invalid command.")
 
